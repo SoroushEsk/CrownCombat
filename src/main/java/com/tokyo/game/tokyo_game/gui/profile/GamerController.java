2f07a499d9587a668590793574e9adb1dc3a16f3
Mr.slow <semp1381@gmail.com>
2024-07-12 11:31:16 +0330

    add back functionality to profile page

--- src/main/java/com/tokyo/game/tokyo_game/gui/profile/GamerController.java
+++ src/main/java/com/tokyo/game/tokyo_game/gui/profile/GamerController.java
@@ -3,13 +3,18 @@ package com.tokyo.game.tokyo_game.gui.profile;
 import com.tokyo.game.tokyo_game.resource.Constants;
 import com.tokyo.game.tokyo_game.user.Gamer;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
-import com.tokyo.game.tokyo_game.cards.Card;
-import java.util.ArrayList;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GamerController {
 
@@ -20,6 +25,8 @@ public class GamerController {
 
     private Gamer gamer;
 
+    @FXML
+    private HBox hBox;
     @FXML
     private void handleUpdateProfile() {
         // Update Gamer properties based on text fields
@@ -37,9 +44,12 @@ public class GamerController {
 
         // Load cards into the TilePane
         cardsPane.getChildren().clear(); // Clear existing cards if any
+        int i = 0;
         for (String card: gamer.getPaths()) {
             ImageView cardView = createCardView(card);
             cardsPane.getChildren().add(cardView);
+            i++;
+            if ( i >= 20) break;
         }
     }
 
@@ -56,6 +66,24 @@ public class GamerController {
 
     public void initialize() {
         gamer = Constants.getInstance().c_gamer; // Or load from a data source
+
+
+        hBox.setOnKeyPressed(event -> {
+            if ( event.getCode() == KeyCode.ESCAPE){
+
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("/com/tokyo/game/tokyo_game/menu.fxml"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                Stage stage = (Stage) hBox.getScene().getWindow();
+                Scene scene = new Scene(root, Constants.getInstance().Windows_Width, Constants.getInstance().Windows_Height);
+                stage.setScene(scene);
+            }
+        });
+
+        hBox.setFocusTraversable(true);
     }
 
 
